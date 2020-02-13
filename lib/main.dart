ccd2280bf613cef744ef703af042b302be4bc460
eugenia troyan <evgenia.troyan@gmail.com>
2020-02-13 15:16:53 +0200

    added firebase messaging init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:custodia/screens/dashboard/dashboard.dart';
 import 'package:custodia/screens/questionnaire/step_home_types.dart';
 import 'package:custodia/screens/splash.dart';
+import 'package:custodia/services/firebase_messaging.dart';
 import 'package:custodia/theme-provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.black, // navigation bar color
   ));
-
+  FirebaseNotifications().setUpFirebase();
 //  FirebaseAuth.instance.signOut();
 
   runApp(MyApp());
@@ -23,7 +25,6 @@ class MyApp extends StatelessWidget {
       title: 'Custodia',
       debugShowCheckedModeBanner: false,
       theme: ThemeProvider.theme,
-//      home: DashboardScreen()
     home: SplashScreen()
     );
   }
