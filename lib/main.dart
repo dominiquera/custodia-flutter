3de20a188436ef5d37e1c1c3bc42c8223ad01d38
Dominique Rademacher <dorademacher@gmail.com>
2020-03-12 17:06:44 +0100

    device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:custodia/screens/dashboard/dashboard.dart';
 import 'package:custodia/screens/questionnaire/step_home_types.dart';
 import 'package:custodia/screens/splash.dart';
 import 'package:custodia/theme-provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:flutter/services.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -19,6 +18,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Custodia',
       debugShowCheckedModeBanner: false,
